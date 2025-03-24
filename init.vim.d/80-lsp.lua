e618d0bb31e4d15bdd2290d6c96cbf7e3295cdac
kheldae <taho.gougeon@gmail.com>
2025-03-24 08:51:05 +0100

    rustls fix

--- init.vim.d/80-lsp.lua
+++ init.vim.d/80-lsp.lua
@@ -100,6 +100,18 @@ lsp_with_coq(lsp.rust_analyzer, {
     cmd = nix:shell("rust-analyzer", { "rust-analyzer" }),
 })
 
+-- Rustls fix
+for _, method in ipairs({
+    'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return -- stfu handler
+        end
+        return default_handler(err, result, context, config)
+    end
+end
+
 -- Haskell
 lsp_with_coq(lsp.hls, {
     cmd = nix:shell("haskell-language-server", {
