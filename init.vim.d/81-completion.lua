176b1ed4c92befa556489b0822c42104fc53256e
kheldae <taho.gougeon@gmail.com>
2025-10-23 10:30:13 +0200

    blink.cmp behavior change

--- init.vim.d/81-completion.lua
+++ init.vim.d/81-completion.lua
@@ -10,10 +10,12 @@ require 'blink.cmp'.setup {
     -- completion behavior
     completion = {
         keyword = { range = 'prefix' },
-        accept = { auto_brackets = { enabled = false } },
-        list = { selection = { preselect = true, auto_insert = false } }
-        -- appearance tweaks
+        accept = { auto_brackets = { enabled = true } },
+        list = { selection = { preselect = true, auto_insert = false } },
+        -- auto documentation popup
+        documentation = { auto_show = true, auto_show_delay_ms = 500 }
     },
+    -- no command completion
     cmdline = { enabled = false },
 
     -- tweaking keymap to ensure acceptance at the right time
@@ -23,7 +25,8 @@ require 'blink.cmp'.setup {
         -- new keybinds to ensure old completion behavior from coq.nvim
         ['<Up>'] = { 'select_prev', 'fallback' },
         ['<Down>'] = { 'select_next', 'fallback' },
-        ['<CR>'] = { 'select_and_accept', 'fallback' }
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
         -- rollbacking completion and deleting compleated text
+        ['<Left>'] = { 'cancel', 'fallback' }
     }
 }
