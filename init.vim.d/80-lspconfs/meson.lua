52777ba592bf88f87d5a29d13fb5ad3ba1bca506
kheldae <taho.gougeon@gmail.com>
2026-01-10 16:48:32 +0100

    adding mesonlsp

---
+++ init.vim.d/80-lspconfs/meson.lua
@@ -0,0 +1,37 @@
+-- MesonLSP config
+
+require 'nix'
+local nix = Nix:new()
+
+-- aux function
+local meson_matcher = function(_, path)
+    local pattern = "meson.build"
+    local f = vim.fn.glob(table.concat({ path, pattern }, '/'))
+    -- pre-emptive check
+    if f == '' then
+        return false
+    end
+    -- checking for meson
+    for line in io.lines(f) do
+        -- skip comments
+        if not line:match '^%s*#.*' then
+            local str = line:gsub('%s+', '')
+            if str ~= '' then
+                return true
+            else
+                break
+            end
+        end
+    end
+    -- failsafe return
+    return false
+end
+
+-- configuring lsp
+vim.lsp.config('mesonlsp', {
+    cmd = nix:shell("mesonlsp", { "mesonlsp", "--lsp" }),
+    filetypes = { "meson" },
+    root_dir = function(bufnr, on_dir)
+        on_dir(vim.fs.root(bufnr, meson_matcher) or vim.fs.root(bufnr, '.git'))
+    end
+})
